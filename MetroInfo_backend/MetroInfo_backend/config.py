9c8ce4d2e6be6573bd26748320efdf5e6077e5a0
zhang-xunxuan <2146455611@qq.com>
2025-03-30 21:02:52 +0800

    初始提交

---
+++ MetroInfo_backend/MetroInfo_backend/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    # 配置数据库连接信息
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Zz13193515431@localhost/smart_metro'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.urandom(24)  # 用于生成会话密钥
